7012e8f281566afac832f030909dd9688c03b5b6
Sanjay <120720616+Alexandero5@users.noreply.github.com>
2023-07-08 10:43:05 +0530

    changes in console

--- console.php
+++ console.php
@@ -58,28 +58,6 @@
           
             <!-- Chart End -->
              <!-- Form Start -->
-             <div class="container-fluid pt-4 px-4">
-                    <div class="row g-4">
-                     
-                        <div class="col-sm-12 col-xl-6">
-                            <div class="bg-light rounded h-100 p-4">
-                                <h6 class="mb-4">Delivery Agents</h6>
-                                <div class="">
-                                    <select class="form-select" id="floatingSelect"
-                                        aria-label="Floating label select example">
-                                        <option
-                                          class="" selected>Select Delivery Agents </option>
-                                        <option value="1">Balu</option>
-                                        <option value="2">Jagadeesh</option>
-                                        <option value="3">Nani</option>
-                                    </select>
-                                </div>
-                
-                            </div>
-                        </div>
-     
-                    </div>
-                </div>
             <!-- Form End -->
 
          
