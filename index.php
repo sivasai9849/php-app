1a815a4e309816e756b75b6123d7749be97eae40
Sanjay <120720616+Alexandero5@users.noreply.github.com>
2023-07-11 10:00:17 +0530

    logo

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION["username"])) {
 
 <head>
     <meta charset="utf-8">
-    <title>DASHMIN - Bootstrap Admin Template</title>
+    <title>Lunch Box</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
--- sidebar.php
+++ sidebar.php
@@ -12,8 +12,8 @@
 ?>
 <div class="sidebar pe-4 pb-3">
     <nav class="navbar bg-light navbar-light">
-        <a href="index.html" class="navbar-brand mx-4 mb-3">
-            <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>LUNCHBOX</h3>
+        <a href="index.php" class="navbar-brand mx-4 mb-3">
+            <h3 class="text-primary"><img src="./img/icon.png" style="width: 60px; height: auto;">LUNCH BOX</h3>
         </a>
         <div class="d-flex align-items-center ms-4 mb-4">
             <div class="position-relative">
