333b21dfb63c0a404d81f7b603202a727eb25cfb
Sanjay <120720616+Alexandero5@users.noreply.github.com>
2023-08-03 15:01:02 +0530

    changes

--- console.php
+++ console.php
@@ -68,14 +68,43 @@
          }
       </style>
    
+
+
+   <div class="container-fluid pt-4 px-4">
+    <div class="bg-light text-center rounded p-4">
+        <div class="d-flex align-items-center justify-content-between mb-4">
+            <h6 class="mb-0">Customers Data</h6>
+            <a href="#">Show All</a>
+        </div>
+        <div class="table-responsive">
+       
+           
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <!-- Horizontal Bar End -->
 </div>
 <!-- Content End -->
-
-
-
-
-        
+  
         
         <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
